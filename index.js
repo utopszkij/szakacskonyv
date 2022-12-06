bb41c56f3b700c1e9ee5cafee23d5345feec88d1
utopszkij <tibor.fogler@gmail.com>
2022-12-06 15:58:12 +0100

    dizájn fejlesztés

--- index.js
+++ index.js
@@ -16,7 +16,7 @@
 		  let name = cname + "=";
 		  let decodedCookie = decodeURIComponent(document.cookie);
 		  let ca = decodedCookie.split(';');
-		  for(let i = 0; i <ca.length; i++) {
+		  for(let i = 0; i < ca.length; i++) {
 			let c = ca[i];
 			while (c.charAt(0) == ' ') {
 			  c = c.substring(1);
--- index.php
+++ index.php
@@ -48,7 +48,7 @@ $statisticModel->saveStatistic();
 $fw = new Fw();
 
 //+ ----------- verzio kezelés start ------------
-$fileVerzio = 'v2.2.0';
+$fileVerzio = 'v2.1.0';
 $upgrade = new \Upgrade();
 $dbverzio  = $upgrade->getDBVersion();
 $lastVerzio = $upgrade->getLastVersion();
